ba8417ec7559e8461f137f4d8fd3d124a926661c
Alma <a.q.frankenstein@gmail.com>
2020-08-21 09:07:37 -0700

    test that form can take a number and return it

---
+++ js/scripts.js
@@ -0,0 +1,45 @@
+// function factorial(userNumber) {
+//   if (userNumber === 0) {
+//     return 1;
+//   }
+//   if (userNumber < 0) {
+//     return;
+//   }
+//   let total = 1;
+//   for (let num = userNumber; num > 0; num -= 1) {
+//     total *= num;
+//   }
+//   return total;
+// }
+
+// Tests 
+// User can input a number and see it returned:
+function returner(userNumber) {
+  return userNumber;
+}
+
+// $(document).ready(function() {
+//   $("#number").submit(function(event) {
+//     event.preventDefault();
+
+//     const userNumber = parseInt($("#inputNum").val());
+
+//     let returnNumber = factorial(userNumber);
+
+//     $("#returnNumber").text(returnNumber);
+//     $("#returnNumber").show();
+//   })
+// });
+
+  $(document).ready(function() {
+    $("#number").submit(function(event) {
+      event.preventDefault();
+  
+      const userNumber = parseInt($("#inputNum").val());
+  
+      let returnNumber = returner(userNumber);
+  
+      $("#returnNumber").text(returnNumber);
+      $("#returnNumber").show();
+    })
+});
